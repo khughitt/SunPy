8f55b7f5268bc361b9bbab09bfdbef167590e3a9
Keith Hughitt <keith.hughitt@gmail.com>
2011-10-07 15:29:11 -0400

    Adjusted adaptive_cmap function work with an existing colormap as a base.

--- sunpy/cm/cm.py
+++ sunpy/cm/cm.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 This module provides a set of colormaps specific to solar data (e.g. SDO/AIA 
 color maps), functions for getting a colormap by name.
 """
-
 import sys
 import numpy as np
 import matplotlib.colors as colors
@@ -11,6 +10,8 @@ import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
 import matplotlib.cm as cm
 from sunpy.cm import _cm
+
+#pylint: disable=E1101
 
 cmlist = {}
 _aia_wavelengths = [94, 131, 171, 193, 211, 304, 335, 1600, 1700, 4500]
@@ -27,7 +28,7 @@ for wl in _eit_wavelengths:
     cmlist[key] = colors.LinearSegmentedColormap(key, _cm.eit_color_table(wl))
     
 # RHESSI
-cmlist['rhessi'] = cm.jet #pylint: disable=E1101
+cmlist['rhessi'] = cm.jet
 
 # Make accessible directly from module
 for k, v in cmlist.items():
@@ -40,49 +41,21 @@ def get_cmap(name):
     else:
         raise ValueError("Colormap %s is not recognized" % name)
     
-#def get_adaptive_cmap():
-
-def show_colormaps():
-    """Displays custom color maps supported in SunPy"""
-    maps = sorted(cmlist)
-    nmaps = len(maps) + 1
+def get_adaptive_cmap(data, cmap=cm.gray, N=256, vmin=None, vmax=None, 
+                      log=False, scale_factor=1.5):
+    """Returns an optimized colormap for the specified data.
     
-    a = np.linspace(0, 1, 256).reshape(1, -1) #pylint: disable=E1103
-    a = np.vstack((a, a))
-    
-    fig = plt.figure(figsize=(5, 10))
-    fig.subplots_adjust(top=0.99, bottom=0.01, left=0.2, right=0.99)
-    for i,name in enumerate(maps):
-        ax = plt.subplot(nmaps, 1, i + 1)
-        plt.axis("off")
-        plt.imshow(a, aspect='auto', cmap=get_cmap(name), origin='lower')
-        pos = list(ax.get_position().bounds)
-        fig.text(pos[0] - 0.01, pos[1], name, fontsize=10, 
-                 horizontalalignment='right')
-
-    plt.show()
-    
-#
-# Things to try:
-#   use sub-sampling to speed up histogram generation
-#
-def adaptive_cmap(data, N=256, vmin=None, vmax=None, log=False, scale_factor=2):
-    """Creates a custom log-scaled color map scaled to the specified image data.
-    
-    In order increase contrast in those pixel ranges which occur
-    frequently a histogram is created for the log of the data and
-    those values which occur most often are given unique colors.
-    
-    The method needs to be tested on other AIA, etc. data. For the sample
-    image the color maps generated tend favor detail in the corona.
-    
-    So far little tweaking has been done though and there are probably
-    things that could improved. Feel free to try!
+    The image data (histogram) is used to construct a custom colormap based on
+    an existing color map. The function works by using the image's histogram
+    to determine where the most frequently occuring pixel values lie. Colormap
+    indices are then weighted more towards those values.
        
     Parameters
     ----------
     data : numpy.ndarray
         Image data
+    cmap : string
+        Name of the color map to use as a base
     N    : int
         Number of colors to use for the color map
     vmin : int
@@ -105,9 +78,8 @@ def adaptive_cmap(data, N=256, vmin=None, vmax=None, log=False, scale_factor=2):
     --------
     >>> map = sunpy.Map(sunpy.AIA_171_IMAGE)
     >>> cmap = adaptive_cmap(map)
-    >>> map.plot(cmap=cmap)    
+    >>> map.plot(cmap=cmap)   
     """
-    
     # Applying clipping and log scaling if applicable
     if vmin is not None:
         data = data.clip(vmin)
@@ -123,39 +95,51 @@ def adaptive_cmap(data, N=256, vmin=None, vmax=None, log=False, scale_factor=2):
     # Scale from 0 to 1
     bins = bins / data.max() 
     
-    # Create a matplotlib-formatted color dictionary
-    cdict = _generate_cdict_for_indices(bins, N)
-    
-    return colors.LinearSegmentedColormap('automap', cdict, N)
+    return _adjust_cmap_indices(cmap, bins, N)
 
-def _generate_cdict_for_indices(indices, cmap_size):
-    """Converts a list of indice values to an RGB color dictionary needed 
-       to generate a linear segmented colormap
-       
-       See: http://matplotlib.sourceforge.net/api/colors_api.html
+def _adjust_cmap_indices(cmap, indices, N):
+    """Creates a copy of a color map with the same interpolation values but
+    new indices.
     """
-    step = 1. / cmap_size
-    cdict = {'red': [], 'green': [], 'blue': []}
+    name = cmap.name
+    new_cmap = {"red": [], "green": [], "blue": []}
     
-    value = 0
-    
-    for i in indices:
-        cmap_value = (i, value, value)
-        cdict['red'].append(cmap_value)
-        cdict['green'].append(cmap_value)
-        cdict['blue'].append(cmap_value)
-        value += step
-        
-    # cmap values must range from 0 to 1
-    cdict['red'][0] = cdict['green'][0] = cdict['blue'][0] = (0, 0, 0)
-    cdict['red'][-1] = cdict['green'][-1] = cdict['blue'][-1] = (1, 1, 1)
-    
-    # convert rgb lists to tuples
-    cdict['red'] = tuple(cdict['red'])
-    cdict['green'] = tuple(cdict['green'])
-    cdict['blue'] = tuple(cdict['blue'])
+    cdict = cmap._segmentdata #pylint: disable=W0212
 
-    return cdict
+    i = 0
+    for index in indices:
+        rval = (index, cdict['red'][i][1], cdict['red'][i][2])
+        new_cmap['red'].append(rval)
+        
+        gval = (index, cdict['green'][i][1], cdict['green'][i][2])
+        new_cmap['green'].append(gval)
+        
+        bval = (index, cdict['blue'][i][1], cdict['blue'][i][2])
+        new_cmap['blue'].append(bval)
+        
+        i += 1
+        
+    return colors.LinearSegmentedColormap(name + "_adaptive", new_cmap, N)
+    
+def show_colormaps():
+    """Displays custom color maps supported in SunPy"""
+    maps = sorted(cmlist)
+    nmaps = len(maps) + 1
+    
+    a = np.linspace(0, 1, 256).reshape(1, -1) #pylint: disable=E1103
+    a = np.vstack((a, a))
+    
+    fig = plt.figure(figsize=(5, 10))
+    fig.subplots_adjust(top=0.99, bottom=0.01, left=0.2, right=0.99)
+    for i,name in enumerate(maps):
+        ax = plt.subplot(nmaps, 1, i + 1)
+        plt.axis("off")
+        plt.imshow(a, aspect='auto', cmap=get_cmap(name), origin='lower')
+        pos = list(ax.get_position().bounds)
+        fig.text(pos[0] - 0.01, pos[1], name, fontsize=10, 
+                 horizontalalignment='right')
+
+    plt.show()
 
 def _get_frequent_values(data, N, scale_factor):
     """
